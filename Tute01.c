176d9819968fc11f8862f35e96d17d4edd0e4b5a
IT21817458 <110730029+IT21817458@users.noreply.github.com>
2022-08-06 14:03:59 +0000

    Answers of the tutorial 01 - IT21817458

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,20 @@
 
 #include <stdio.h>
 
-int main() {
-  
+int main() 
+{
+  int subject1, subject2;
+  double average;
+
+  printf("Enter the mark 1 :");
+  scanf("%d", &subject1);
+
+  printf("Enter the mark 2 :");
+  scanf("%d", &subject2);
+
+  average = (subject1 + subject2) / 2.0;
+
+  printf("Average : %.2f", average);
   return 0;
 }
 
--- Tute03.c
+++ Tute03.c
@@ -12,8 +12,19 @@ n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
-int main() {
-  
-  return 0;
-}
 
+int main()
+{
+    int i, x, sum=0;
+
+    printf("Input the number : ");
+    scanf("%d", &x);
+
+    for(i=1; i<=x; i++){
+        sum += i;
+    }
+
+    printf("Sum  = %d", sum);
+
+    return 0;
+}
